d67d879eaf0bb379f5bd73e2b0542c636677b220
Utomwen Imasuen <utomwenimasuen@utomwens-mini.home>
2021-10-02 00:29:37 +0100

    contact form

--- pages/contact.js
+++ pages/contact.js
@@ -48,6 +48,40 @@ function contact() {
               <div className="border-[1px] border-blue-800 w-20"></div>
             </div>
             {/* contact form */}
+            <div className="flex flex-col">
+              <div className="flex flex-wrap md:space-x-8 items-center space-y-4 md:space-y-0">
+                <div className="md:w-1/2 w-full  flex flex-col space-y-4">
+                  {/* input */}
+                  <input
+                    type="text"
+                    placeholder="Full Name"
+                    className="w-full border-[2px] border-gray-300 rounded focus:outline-none placeholder-gray-300 p-2 text-black"
+                  />
+                  <input
+                    type="text"
+                    placeholder="Email Address"
+                    className="w-full border-[2px] border-gray-300 rounded focus:outline-none placeholder-gray-300 p-2 text-black"
+                  />
+                  <input
+                    type="text"
+                    placeholder="Subject"
+                    className="w-full border-[2px] border-gray-300 rounded focus:outline-none placeholder-gray-300 p-2 text-black"
+                  />
+                </div>
+                <div className="flex flex-1 max-w-full md:w-1/2">
+                  {/* textarea */}
+                  <textarea
+                    rows={6}
+                    placeholder="Message"
+                    className="w-full focus:outline-none border-[2px] border-gray-300 placeholder-gray-300 p-2 rounded"
+                  />
+                </div>
+              </div>
+
+              <div className="my-4 text-center text-black px-6 py-2 border-2 rounded-3xl cursor-pointer border-blue-900 w-44 bg-white">
+                Send message
+              </div>
+            </div>
           </div>
         </div>
       </div>
